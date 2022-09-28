5e9e4b7c0e99bbdc89f39322d5d7198e42587900
Dosztosz <dosztosz@int.pl>
2022-09-28 11:40:20 +0200

    first update

---
+++ main.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php require "head.php";?>
+<body>
+    <div class="container">
+        <div class="navbar">
+            <ul>
+                <li><a>Mapa</a></li>
+                <li><a>Misje</a></li>
+                <li><a>Surowce</a></li>
+                <li><a>Żaba</a></li>
+                <li><a>Ustawienia</a></li>
+            </ul>
+        </div>
+        <div class="main">
+            <div class="info-bar">
+                <ul>
+                    <li>Witaj ?Player?</li>
+                    <li><img src="#" alt="#"></li>
+                    <li><a href="#">Ustawienia</a></li>
+                    <li><a href="#">Wyloguj</a></li>
+                </ul>
+            </div>
+            <div class="content">
+                <div class="frog-pic">
+
+                </div>
+                <div class="statistics">
+                    <ul>
+                        <li>Statystyki Żaby</li>
+                        <li>Poziom: </li>
+                        <li>Zdrowie: </li>
+                        <li>Siła: </li>
+                        <li>Lepkość: </li>
+                        <li>Słodkość: </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
