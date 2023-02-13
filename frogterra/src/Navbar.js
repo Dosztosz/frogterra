d2ae29fd6da567036017b9ee65c6dab8c188e0b6
Dosztosz <dosztosz@int.pl>
2023-02-13 11:05:12 +0100

    up

--- frogterra/src/Navbar.js
+++ frogterra/src/Navbar.js
@@ -1,6 +1,6 @@
 
 
-import { Link, useMatch, useResolvedPath, useMatch } from "react-router-dom"
+import { Link, useMatch, useResolvedPath } from "react-router-dom"
 export default function Navbar(){
     return <nav className="nav">
         <Link to="/" className="site-title">Site Name</Link>
@@ -17,7 +17,7 @@ function CustomLink({to , children, ...props}){
     const isActive = useMatch({ path: resolvedPath.pathname, end: true})
      
     return (
-        <li className={path === to ? "active" : ""}>
+        <li className={isActive ? "active" : ""}>
             <Link to={to} {...props}>{children}</Link></li>
     )
 }
